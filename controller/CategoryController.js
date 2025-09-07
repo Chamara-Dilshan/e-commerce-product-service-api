6fc2caaa5d0deb6436a7d235ad8794c9aa446834
Chamara Dilshan <chamaradilshan0011@gmail.com>
2025-09-08 01:02:20 +0530

    added category creation

--- controller/CategoryController.js
+++ controller/CategoryController.js
@@ -1,12 +1,38 @@
 const CategorySchema = require('../model/CategorySchema');
 
-//save
+//create (POST)
 const createCategory = async (request, response) => {
     console.log("Hi We are gonna print the Body Data");
-    console.log(request.body);
+    const category=new CategorySchema({
+        categoryName:request.body.categoryName,
+        icon:{
+            hash:'Temp Hash', 
+            resourceUrl:'https://upload.wikimedia.org/wikipedia/en/thumb/5/5f/Original_Doge_meme.jpg/250px-Original_Doge_meme.jpg', 
+            fileName:'Temp File Name', 
+            directory:'Temp Directory'
+        },
+        availableCountries:[
+            {
+                countryId:'Temp-Id-1',
+                countryName:'Sri Lanka'
+            },
+            {
+                countryId:'Temp-Id-1',
+                countryName:'Sri Lanka'
+            },
+        ]
+    });
+    category.save().then((result)=>{
+        console.log(result);
+        response.status(201).json({code:201, message:'Category Created Successfully', data:result});
+    }).catch((error)=>{
+        console.log(error);
+        response.status(500).json({code:500, message:'Category Creation Failed', error:error});
+    })
+
 };
 
-//update
+//update (PUT)
 const updateCategory = async (request, response) => {
     console.log(request.body);
 };
