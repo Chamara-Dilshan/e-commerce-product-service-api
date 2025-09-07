68e0ccc058717e53e9d290e85ca77d0bccaa8b49
Chamara Dilshan <chamaradilshan0011@gmail.com>
2025-09-08 02:46:37 +0530

    updated

--- controller/CategoryController.js
+++ controller/CategoryController.js
@@ -2,34 +2,45 @@ const CategorySchema = require('../model/CategorySchema');
 
 //create (POST)
 const createCategory = async (request, response) => {
-    console.log("Hi We are gonna print the Body Data");
-    const category=new CategorySchema({
-        categoryName:request.body.categoryName,
-        icon:{
-            hash:'Temp Hash', 
-            resourceUrl:'https://upload.wikimedia.org/wikipedia/en/thumb/5/5f/Original_Doge_meme.jpg/250px-Original_Doge_meme.jpg', 
-            fileName:'Temp File Name', 
-            directory:'Temp Directory'
-        },
-        availableCountries:[
-            {
-                countryId:'Temp-Id-1',
-                countryName:'Sri Lanka'
+
+    try {
+
+        const {categoryName, file, countryId} = request.body;
+        if (!categoryName || !file || !countryId) {
+            return response.status(400).json({code: 400, message: 'Required fields are missing', data: null});
+        }
+
+        console.log("Hi We are gonna print the Body Data");
+
+        const category=new CategorySchema({
+            categoryName:categoryName,
+            icon:{
+                hash:'Temp Hash', 
+                resourceUrl:'https://upload.wikimedia.org/wikipedia/en/thumb/5/5f/Original_Doge_meme.jpg/250px-Original_Doge_meme.jpg', 
+                fileName:'Temp File Name', 
+                directory:'Temp Directory'
             },
-            {
-                countryId:'Temp-Id-1',
-                countryName:'Sri Lanka'
-            },
-        ]
-    });
-    category.save().then((result)=>{
+            availableCountries:[
+                {
+                    countryId:'Temp-Id-1',
+                    countryName:'Sri Lanka'
+                },
+                {
+                    countryId:'Temp-Id-1',
+                    countryName:'Sri Lanka'
+                },
+            ]
+            });
+
+        const saveData = await category.save();
         console.log(result);
         response.status(201).json({code:201, message:'Category Created Successfully', data:result});
-    }).catch((error)=>{
+            
+    } catch (error) {
         console.log(error);
         response.status(500).json({code:500, message:'Category Creation Failed', error:error});
-    })
-
+    }
+   
 };
 
 //update (PUT)
